b2b71bf8a498864b3f7bee638e171874b87498e2
Adii0906 <spliots09@gmail.com>
2026-03-01 22:25:57 +0530

    Two-Pointers:Arr

--- Pratice.py
+++ Pratice.py
@@ -1,25 +1,60 @@
-arr = [1, 2, 3, 4, 5]
+# arr = [1, 2, 3, 4, 5]
 
-print("Array:", arr)
-print("Length of array:", len(arr))
-print("Last index:", len(arr) - 1)
+# print("Array:", arr)
+# print("Length of array:", len(arr))
+# print("Last index:", len(arr) - 1)
 
-left= 0
-right= len(arr)-1
-while left < right:
-    arr[left], arr[right] = arr[right], arr[left]
-    left += 1
-    right -= 1
-print(arr)
+# left= 0
+# right= len(arr)-1
+# while left < right:
+#     arr[left], arr[right] = arr[right], arr[left]
+#     left += 1
+#     right -= 1
+# print(arr)
 
 
-arr = [1,2,3,4,5,6]
-target = 6
+# arr = [1,2,3,4,5,6]
+# target = 6
 
-n = len(arr)
+# n = len(arr)
 
-for i in range(n):
-    for j in range(i + 1, n):
-        if arr[i] + arr[j] == target:
-            print("Target found at indices:", i, j)
-            print("Values:", arr[i], arr[j])
+
+# Sample Two Sum Problem
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         if arr[i] + arr[j] == target:
+#             print("Target found at indices:", i, j)
+#             print("Values:", arr[i], arr[j])
+
+from multiprocessing import Value
+import numbers
+from tkinter import LEFT
+from turtle import left
+
+# To swap an numbers
+arr=[1,2,34,5]
+left=0
+right=len(arr)-1
+print("lenght",l)
+print("right",right)
+while left<right:
+    left[arr],right[arr],right[arr],left[arr]
+    print(arr)
+
+# To find the max Value from an given array
+MAX=0
+for i in range(len(arr)):
+    if arr[i]>MAX:
+        print("This is the max value")
+    else:
+        print("This is small value")
+exit
+
+MIN=0
+for i in range(len(arr)):
+    if arr[i]<MIN:
+        print("This is the min value")
+    else:
+        print("No  found")
+exit
+
